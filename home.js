0fb269befdbc2f0761465514131a55fbc613a53b
LuayB <luaboett@gmail.com>
2024-03-02 11:27:24 +0000

    Add some default configurations to the editor

--- home.js
+++ home.js
@@ -1,1 +1,7 @@
 var editor = ace.edit("editor");
+
+// Editor configurations
+
+editor.session.setMode("ace/mode/python");
+editor.setValue("");
+document.getElementById('editor').style.fontSize='20px';
